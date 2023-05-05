3b38ccf81e7cd256102ca5b63f0965b48a6d66ab
jeonga127 <j970924@gmail.com>
2023-05-06 01:37:20 +0900

    Feat : Spring Security 적용

---
+++ src/main/java/com/example/getreadyauction/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.getreadyauction.security;
+
+import com.example.getreadyauction.entity.Users;
+import com.example.getreadyauction.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsersRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(user, user.getUsername());
+    }
+
+}
