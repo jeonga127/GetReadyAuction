1a6cb60f476339635a426f71cfdde137a1965702
jeonga127 <j970924@gmail.com>
2023-05-06 23:05:39 +0900

    Fixes : 1차 테스트(3)

--- src/main/java/com/example/getreadyauction/controller/UserController.java
+++ src/main/java/com/example/getreadyauction/controller/UserController.java
@@ -20,19 +20,16 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/signup")//회원가입 페이지 보여주기(?)
-    public ModelAndView signupPage() {
-        return new ModelAndView("signup");
-    }
-
-    @GetMapping("/login")//로그인 페이지 보여주기(?)
-    public ModelAndView loginPage() {
-        return new ModelAndView("login");
-    }
-
-
-
-
+//    추후 프론트와 합칠 때 살릴 것
+//    @GetMapping("/signup")//회원가입 페이지 보여주기(?)
+//    public ModelAndView signupPage() {
+//        return new ModelAndView("signup");
+//    }
+//
+//    @GetMapping("/login")//로그인 페이지 보여주기(?)
+//    public ModelAndView loginPage() {
+//        return new ModelAndView("login");
+//    }
 
     @PostMapping("/signup")//회원가입
     public LoginResponseDto signup(@Valid @RequestBody SignupRequestDto signupRequestDto, BindingResult bindingResult) {
--- src/main/java/com/example/getreadyauction/dto/auction/AuctionResponseDto.java
+++ src/main/java/com/example/getreadyauction/dto/auction/AuctionResponseDto.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class AuctionResponseDto {
+    private Long id;
     private String title;
     private String category;
     private int currentPrice;
     private LocalDateTime deadline;
 
     public AuctionResponseDto(Auction auction){
+        this.id = auction.getId();
         this.title = auction.getTitle();
         this.category = auction.getCategory();
         this.currentPrice = auction.getCurrentPrice();
