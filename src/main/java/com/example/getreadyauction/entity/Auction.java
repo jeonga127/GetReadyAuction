4fbed22e646b54dccfeb790021d06e75feb56b0a
youngkikim <rkfcl1169@gmail.com>
2023-05-06 13:44:35 +0900

    feat : auction.CUD add

--- src/main/java/com/example/getreadyauction/entity/Auction.java
+++ src/main/java/com/example/getreadyauction/entity/Auction.java
@@ -1,5 +1,6 @@
 package com.example.getreadyauction.entity;
 
+import com.example.getreadyauction.dto.AuctionRequestDto;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -45,7 +46,7 @@ public class Auction extends Timestamped {
     private Users user;
 
     @Builder
-    public Auction(String title, String category, String content, int minPrice, String deadline, Users user){
+    public Auction(AuctionRequestDto auctionRequestDto, Users user){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초");
 
         this.title = title;
@@ -61,5 +62,21 @@ public class Auction extends Timestamped {
 
     public void setCurrentPrice(int currentPrice){
         this.currentPrice = currentPrice;
+    }
+
+    public void Edit(AuctionRequestDto auctionRequestDto) {
+        this.title = auctionRequestDto.getTitle();
+        this.category = auctionRequestDto.getCategory();
+        this.content = auctionRequestDto.getContent();
+        this.minPrice = auctionRequestDto.getMinPrice();
+        this.deadline = auctionRequestDto.getDeadline();
+    }
+
+    public void Up(Auction auction){
+        this.title = auction.getTitle();
+        this.category = auction.getCategory();
+        this.content = auction.getContent();
+        this.minPrice = auction.getMinPrice();
+        this.deadline = auction.getDeadline();
     }
 }
--- src/main/java/com/example/getreadyauction/service/AuctionService.java
+++ src/main/java/com/example/getreadyauction/service/AuctionService.java
@@ -1,12 +1,23 @@
 package com.example.getreadyauction.service;
 
 import com.example.getreadyauction.dto.AuctionCategoryDto;
+import com.example.getreadyauction.dto.AuctionRequestDto;
 import com.example.getreadyauction.dto.AuctionSearchDto;
 import com.example.getreadyauction.dto.ResponseDto;
+import com.example.getreadyauction.entity.Auction;
+import com.example.getreadyauction.entity.Users;
+import com.example.getreadyauction.repository.AuctionRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuctionService {
+
+    private final AuctionRepository auctionRepository;
+
+
     public ResponseDto getAllAuctions() {
         return null;
     }
@@ -21,5 +32,46 @@ public class AuctionService {
 
     public ResponseDto getSearchedAuction(AuctionSearchDto auctionSearchDto) {
         return null;
+    }
+
+    public ResponseDto postAddAuction(AuctionRequestDto auctionRequestDto, Users users){
+        Auction auction = new Auction(auctionRequestDto, users);
+        auctionRepository.saveAndFlush(auction);
+        return ResponseDto.setSuccess("물품이 등록되었습니다", null);
+    }
+
+    @Transactional
+    public ResponseDto putEditAuction(Long id, AuctionRequestDto auctionRequestDto, Users users){
+        Auction auction = auctionRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("존재하지 않는 물품입니다")  // null 처리
+        );
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
+            auction.Edit(auctionRequestDto);
+        } else
+            throw new IllegalArgumentException("권한이 없습니다");
+        return ResponseDto.setSuccess("물품이 수정되었습니다", null);
+    }
+    @Transactional
+    public ResponseDto putUpAuction(Long id, Users users){
+        Auction auction = auctionRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("존재하지 않는 물품입니다.") // null 처리
+        );
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
+            auction.Up(auction);
+        } else
+            throw new IllegalArgumentException("권한이 없습니다");
+        return ResponseDto.setSuccess("끌어 올려졌습니다!", null);
+    }
+
+    @Transactional
+    public ResponseDto delAuction(Long id, Users users){
+        Auction auction = auctionRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("존재하지 않는 물품입니다.") // null 처리
+        );
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
+            auctionRepository.deleteById(id);
+        } else
+            throw new IllegalArgumentException("권한이 없습니다");
+        return ResponseDto.setSuccess("물품이 삭제되었습니다.", null);
     }
 }
