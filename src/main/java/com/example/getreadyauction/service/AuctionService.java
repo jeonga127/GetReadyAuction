f0c41e1021aa43138702f15cde7814e6fe9e56ae
youngkikim <rkfcl1169@gmail.com>
2023-05-06 17:28:01 +0900

    feat : BiddingService "post" add and add comment

--- src/main/java/com/example/getreadyauction/service/AuctionService.java
+++ src/main/java/com/example/getreadyauction/service/AuctionService.java
@@ -34,44 +34,44 @@ public class AuctionService {
         return null;
     }
 
-    public ResponseDto postAddAuction(AuctionRequestDto auctionRequestDto, Users users){ // 물품 등록
-        Auction auction = new Auction(auctionRequestDto, users);
-        auctionRepository.saveAndFlush(auction);
+    public ResponseDto postAddAuction(AuctionRequestDto auctionRequestDto, Users users){ // 저장 서비스
+        Auction auction = new Auction(auctionRequestDto, users); // Requestdto 받아서 user 정보와 함께 객체 생성
+        auctionRepository.saveAndFlush(auction); // 객체를 레포지토리에 저장
         return ResponseDto.setSuccess("물품이 등록되었습니다", null);
     }
 
     @Transactional
-    public ResponseDto putEditAuction(Long id, AuctionRequestDto auctionRequestDto, Users users){ //물품 수정
+    public ResponseDto putEditAuction(Long id, AuctionRequestDto auctionRequestDto, Users users){ // 수정 서비스
         Auction auction = auctionRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 물품입니다")  // null 처리
+                () -> new NullPointerException("존재하지 않는 물품입니다")  // id값을 받아서 존재하는 물품인지 찾아서 담아줌, 혹시 없으면 null 처리
         );
-        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
-            auction.Edit(auctionRequestDto);
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 물품 등록자의 id와 수정하려는 사람의 id를 가져와서 비교
+            auction.Edit(auctionRequestDto); // 맞으면 수정해줌
         } else
-            throw new IllegalArgumentException("권한이 없습니다");
+            throw new IllegalArgumentException("권한이 없습니다"); // id 다르면 던져줌
         return ResponseDto.setSuccess("물품이 수정되었습니다", null);
     }
     @Transactional
     public ResponseDto putUpAuction(Long id, Users users){ // 끌올
         Auction auction = auctionRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 물품입니다.") // null 처리
+                () -> new NullPointerException("존재하지 않는 물품입니다.") // id값을 받아서 존재하는 물품인지 찾아서 담아줌, 혹시 없으면 null 처리
         );
-        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
-            auction.Up(auction);
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 물품 등록자의 id와 수정하려는 사람의 id를 가져와서 비교
+            auction.Up(auction); // 맞으면 그냥 담아온 값을 고대로 반영
         } else
-            throw new IllegalArgumentException("권한이 없습니다");
+            throw new IllegalArgumentException("권한이 없습니다"); // id가 다르면 던져줌
         return ResponseDto.setSuccess("끌어 올려졌습니다!", null);
     }
 
     @Transactional
     public ResponseDto delAuction(Long id, Users users){ // 물품 삭제
         Auction auction = auctionRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 물품입니다.") // null 처리
+                () -> new NullPointerException("존재하지 않는 물품입니다.") // id값을 받아서 존재하는 물품인지 찾아서 담아줌, 혹시 없으면 null 처리
         );
-        if (users.getUsername().equals(auction.getUser().getUsername())){ // 게시글의 아이디와 유저의 아이디 비교
-            auctionRepository.deleteById(id);
+        if (users.getUsername().equals(auction.getUser().getUsername())){ // 물품 등록자의 id와 수정하려는 사람의 id를 가져와서 비교
+            auctionRepository.deleteById(id); // 맞으면 받아온 id값을 기반으로 레포지토리에서 삭제
         } else
-            throw new IllegalArgumentException("권한이 없습니다");
+            throw new IllegalArgumentException("권한이 없습니다"); // id 다르면 던져줌
         return ResponseDto.setSuccess("물품이 삭제되었습니다.", null);
     }
 }
--- src/main/java/com/example/getreadyauction/service/BiddingService.java
+++ src/main/java/com/example/getreadyauction/service/BiddingService.java
@@ -23,20 +23,40 @@ public class BiddingService {
 
     @Transactional
     public ResponseDto postBidding(Long id, BiddingRequestDto biddingRequestDto, Users users){
-        Auction auction = auctionRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 물품입니다")  // null 처리
+        Auction auction = auctionRepository.findById(id).orElseThrow( // 게시물이 존재하는지 여부 파악, 존재하면 담아줌
+                () -> new NullPointerException("존재하지 않는 물품입니다")
         );
         if (users.getUsername().equals(auction.getUser().getUsername())) { // 물품 등록자와 입찰자 검증
             throw new IllegalArgumentException("당신은 물품 원주인 입니다...");
         }
-        Optional<Bidding> bidding = biddingRepository.findByIdAndUser(id, users);
-        if (bidding.isEmpty()){
-            biddingRepository.saveAndFlush(new Bidding(biddingRequestDto, users, auction));
-        } else {
-            Bidding editbid = new Bidding();
-            editbid.Edit(biddingRequestDto);
-            return ResponseDto.setSuccess("입찰가 변경!", null);
+
+        Optional<Bidding> biddingFind = biddingRepository.findByAuctionAndUser(auction, users);  // 물품에 자기의 입찰여부를 파악하기 위해 가져옴
+        Optional<Bidding> biddingExist = biddingRepository.findByAuction(auction);  // 물품 자체의 입찰여부를 파악하기 위해 가져옴
+
+        if (biddingExist.isEmpty()) { // 물품에 입찰이 아무것도 없을때
+            if (biddingRequestDto.getPrice() >= auction.getMinPrice()) { // 최소입찰가격과 같거나 크다면 입찰 가능하도록
+                biddingRepository.saveAndFlush(new Bidding(biddingRequestDto, users, auction));
+                return ResponseDto.setSuccess("입찰 성공!", null);
+            } else {
+                throw new IllegalArgumentException("최소 입찰가 이상의 금액을 입력해주세요");
+            }
         }
-        return ResponseDto.setSuccess("입찰 성공!", null);
+
+        if (biddingExist.isPresent()) { // 물품에 입찰이 있는데 자기는 입찰을 하지 않은 경우, 물품에 등록된 기존 입찰 가격보다 큰 값을 적었을 때
+            if (biddingFind.isEmpty() && biddingRequestDto.getPrice() > biddingExist.get().getPrice()) {
+                biddingRepository.saveAndFlush(new Bidding(biddingRequestDto, users, auction));
+                return ResponseDto.setSuccess("입찰 성공!", null);
+            } else {
+                throw new IllegalArgumentException("최고 입찰가 이상의 금액을 입력해주세요");
+            }
+        }
+
+        if (biddingFind.isPresent() && biddingRequestDto.getPrice() > biddingExist.get().getPrice()) { // 내가 입찰을 이미 했고 또 기존 입찰가격보다 더 큰 값을 적었을때
+            Bidding editbid = new Bidding(); // 빈 객체 만들어주고
+            editbid.Edit(biddingRequestDto);  // 수정값을 담아서 수정함
+            return ResponseDto.setSuccess("입찰가 변경!", null);
+        } else {
+            throw new IllegalArgumentException("최고 입찰가 이상의 금액을 입력해주세요");
+        }
     }
 }
