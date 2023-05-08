93992ee0897f751e29c52244310a0f344f442a0a
inggaeng <kce0_0@naver.com>
2023-05-08 11:38:44 +0900

    Fix : 조회 수정

--- src/main/java/com/example/getreadyauction/service/MainService.java
+++ src/main/java/com/example/getreadyauction/service/MainService.java
@@ -3,8 +3,6 @@ package com.example.getreadyauction.service;
 
 import com.example.getreadyauction.dto.MainAuctionDto;
 import com.example.getreadyauction.dto.ResponseDto;
-import com.example.getreadyauction.dto.auction.AuctionCategoryDto;
-import com.example.getreadyauction.dto.auction.AuctionResponseDto;
 import com.example.getreadyauction.entity.Auction;
 import com.example.getreadyauction.repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +32,10 @@ public class MainService {
     @Transactional(readOnly = true) //마감임박 기준 실시간 경매리스트
     public ResponseDto<List<MainAuctionDto>> mainDeadline( Pageable pageable) {
         List<Auction> auctionList = auctionRepository.findAllByOrderByDeadlineAsc(pageable).getContent();
-        List<MainAuctionDto> mainAuctionDto = auctionList.stream().map(MainAuctionDto::new).collect(Collectors.toList());
+        List<MainAuctionDto> mainAuctionDto = auctionList
+                .stream()
+                .map(MainAuctionDto::new)
+                .collect(Collectors.toList());
         return ResponseDto.setSuccess("Success : get All Categorized Auctions Information", mainAuctionDto);
     }
 
@@ -46,5 +46,21 @@ public class MainService {
         List<MainAuctionDto> mainAuctionDto = auctionList.stream().map(MainAuctionDto::new).collect(Collectors.toList());
         return ResponseDto.setSuccess("Success : get All Categorized Auctions Information", mainAuctionDto);
     }
+
+//    @Transactional(readOnly = true)
+//    public ResponseDto<List<MainAuctionDto>> main (String orderBy, Pageable pageable) {
+//        List<Auction> auctionList;
+//        if (orderBy.equals("views")) {
+//            auctionList = auctionRepository.findAllByOrderByViewsAsc(pageable).getContent();
+//        } else if (orderBy.equals("deadline")) {
+//            auctionList = auctionRepository.findAllByOrderByDeadlineAsc(pageable).getContent();
+//        } else if (orderBy.equals("count")) {
+//            auctionList = auctionRepository.findAllByOrderByBidSizeAsc(pageable).getContent();
+//        } else {
+//            throw new IllegalArgumentException("Invalid orderBy value: " + orderBy);
+//        }
+//        List<MainAuctionDto> mainAuctionDto = auctionList.stream().map(MainAuctionDto::new).collect(Collectors.toList());
+//        return ResponseDto.setSuccess("Success : get All Categorized Auctions Information", mainAuctionDto);
+//    }
 
 }
