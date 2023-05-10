06ce58185e5f1b1c90709e7e3906382b8ea1ffe0
jeonga127 <j970924@gmail.com>
2023-05-11 06:15:47 +0900

    Test : CI/CD 배포 test(3)

--- src/test/java/com/example/getreadyauction/GetReadyAuctionApplicationTests.java
+++ src/test/java/com/example/getreadyauction/GetReadyAuctionApplicationTests.java
@@ -3,9 +3,8 @@ package com.example.getreadyauction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GetReadyAuctionApplicationTests {
-
     @Test
     void contextLoads() {
     }
